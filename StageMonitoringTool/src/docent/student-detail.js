9ff0ca646e741be0a6138944dc12dd849df40ae8
adyousfi <adamyousfi789@gmail.com>
2026-06-09 10:18:01 +0200

    feat: actie cards toegevoegd

--- StageMonitoringTool/src/docent/student-detail.js
+++ StageMonitoringTool/src/docent/student-detail.js
@@ -40,6 +40,36 @@ function renderOverzicht(student) {
           <p class="sd-stat-sub">${student.functie}</p>
         </div>
       </div>
+
+      <div class="sd-actie-grid">
+        <div class="sd-actie-card" data-actie="logboek">
+          <div class="sd-actie-icon sd-actie-icon--blauw">
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+              <polyline points="14 2 14 8 20 8"></polyline>
+              <line x1="16" y1="13" x2="8" y2="13"></line>
+              <line x1="16" y1="17" x2="8" y2="17"></line>
+              <polyline points="10 9 9 9 8 9"></polyline>
+            </svg>
+          </div>
+          <div>
+            <p class="sd-actie-titel">Logboek Controleren</p>
+            <p class="sd-actie-sub">Bekijk dagelijkse activiteiten</p>
+          </div>
+        </div>
+        <div class="sd-actie-card" data-actie="evaluatie">
+          <div class="sd-actie-icon sd-actie-icon--paars">
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M9 11l3 3L22 4"></path>
+              <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path>
+            </svg>
+          </div>
+          <div>
+            <p class="sd-actie-titel">Evaluaties</p>
+            <p class="sd-actie-sub">Geef feedback en beoordeling</p>
+          </div>
+        </div>
+      </div>
     </div>
   `;
 }
