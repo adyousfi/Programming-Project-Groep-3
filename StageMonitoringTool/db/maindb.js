414f17270b99aba85223b5d33327b5c45a2c4964
Jon <hysenaj.jon@hotmail.com>
2026-06-09 13:48:12 +0200

    changed strategy
    seeddb is hernieuwd en zal constant werken.
    Je kan nu een stage aanmaken.
    en hier en daar enkele correcties toegevoegd.

--- StageMonitoringTool/db/maindb.js
+++ StageMonitoringTool/db/maindb.js
@@ -4,7 +4,7 @@ import stage from "./objectModel/stage.js";
 import userController from "./userControllers/userController.js";
 import express from 'express';
 import User from '../db/userModel/user.js';
-
+import stageController from "./objectControllers/stageController.js";
 
 await run();
 
@@ -27,7 +27,9 @@ await seedDatabase();
 router.post("/create-user", userController.createUser)
 router.get("/select-user", userController.selectUser)
 
+router.post("/create-stage",stageController.createStage)
+
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`🚀 Server is successfully running on http://localhost:${PORT}`);
+    console.log(`Server is successfully running on http://localhost:${PORT}`);
 });
--- StageMonitoringTool/db/objectControllers/stageController.js
+++ StageMonitoringTool/db/objectControllers/stageController.js
@@ -1,16 +1,35 @@
 import Stage from "../objectModel/stage.js";
 import Stagementor from "../userModel/stagementor.js";
+import { sequelize } from "../dbConnection.js";
 
-const createStage = async (omschrijving_opdracht,begin_datum,eind_datum) =>{
+const createStage = async (req,res,next) =>{
 
-    const bedrijf = await Bedrijf.create({
-        omschrijving_opdracht: omschrijving_opdracht,
-        begin_datum: begin_datum,
-        eind_datum: eind_datum
-    })
-
-    console.log(bedrijf);
+    const {
+        omschrijving_opdracht,
+        status,
+        begin_datum,
+        eind_datum
+    } = req.body;
         
+    try{
+        const user = await Stage.create({
+            omschrijving_opdracht: omschrijving_opdracht,
+            status: status,
+            begin_datum: begin_datum,
+            eind_datum: eind_datum
+        });
+        return res.status(200).json({
+            msg: "Stage created successfully",
+            data: Stage
+        })
+    }
+    catch(error){
+        console.error("Error creating stage: ", error); 
+        return res.status(500).json({
+            msg: "something went wrong while creating stage"
+        });
+
+    }
 }
 
 const linkStageToMoreUser = async (stageId,studentId,docentId,mentorId,bedrijfId,) =>{
@@ -24,7 +43,7 @@ const linkStageToMoreUser = async (stageId,studentId,docentId,mentorId,bedrijfId
     {where: { stage_id: stageId }}
 );
 }
-export {createStage, linkStageToMoreUser};
+export default {createStage, linkStageToMoreUser};
 
 
 
@@ -34,47 +53,32 @@ export {createStage, linkStageToMoreUser};
 
 
 
-const createStage = async (req, res, next) => {
-    const {
-        omschrijving_opdracht,
-        begin_datum,
-        eind_datum
-    } = req.body;
+// const createStage = async (req, res, next) => {
+//     const {
+//         omschrijving_opdracht,
+//         begin_datum,
+//         eind_datum
+//     } = req.body;
 
-    try {
+//     try {
 
-        const user = await User.create({
-            omschrijving_opdracht: omschrijving_opdracht,
-            begin_datum: begin_datum,
-            eind_datum: eind_datum,
-        });
+//         const user = await User.create({
+//             omschrijving_opdracht: omschrijving_opdracht,
+//             begin_datum: begin_datum,
+//             eind_datum: eind_datum,
+//         });
 
         
 
-        return res.status(200).json({
-            msg: "Stage created successfully",
-            data: stage
-        });
+//         return res.status(200).json({
+//             msg: "Stage created successfully",
+//             data: stage
+//         });
 
-    } catch (error) {
-        console.error("Error creating user: ", error); 
-        return res.status(500).json({
-            msg: "something went wrong while creating user"
-        });
-    }
-};
-
-const selectUser = async (req, res, next) => {
-    try {
-        const users = await User.findAll();
-        return res.status(200).json({
-            msg: "Users selected successfully",
-            data: users
-        });
-    } catch (error) {
-        console.error("Error selecting users: ", error);
-        return res.status(500).json({
-            msg: "something went wrong while selecting user"
-        });
-    } 
-};
+//     } catch (error) {
+//         console.error("Error creating user: ", error); 
+//         return res.status(500).json({
+//             msg: "something went wrong while creating user"
+//         });
+//     }
+// };
