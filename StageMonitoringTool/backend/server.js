529e33b1af6ef7a32e752a73ff49cba686ec7caf
adyousfi <adamyousfi789@gmail.com>
2026-06-18 15:02:30 +0200

    Update server.js

--- StageMonitoringTool/backend/server.js
+++ StageMonitoringTool/backend/server.js
@@ -23,7 +23,7 @@ import evaluatieRoutes from '../db/routes/evaluatieRoutes.js';
 const app = express();
 
 dotenv.config();
-await seedDatabase();
+//await seedDatabase();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
